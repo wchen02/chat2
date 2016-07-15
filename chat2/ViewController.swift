a8058b59073f47aee9aabbc4feefc38a11f17dd5
Wensheng Chen <wensheng.chen@nytweb.com>
2016-07-15 18:04:11 -0400

    Initial Commit

---
+++ chat2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat2
+//
+//  Created by yan on 7/15/16.
+//  Copyright © 2016 nytweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ chat2Tests/chat2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  chat2Tests.swift
+//  chat2Tests
+//
+//  Created by yan on 7/15/16.
+//  Copyright © 2016 nytweb. All rights reserved.
+//
+
+import XCTest
+@testable import chat2
+
+class chat2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
